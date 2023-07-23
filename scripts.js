eca18f58b4aad4910aec713081282ba41f58eb44
zixi39 <dev.zixi@gmail.com>
2023-07-22 20:23:10 -0400

    Add button animation

--- scripts.js
+++ scripts.js
@@ -20,4 +20,31 @@ function switchHover() {
     enableHover()
 }
 
+window.onload = function(){
+    window.setTimeout(function(){
+        document.getElementById('twitter-button').style.visibility = 'visible';
+        document.getElementById('twitter-button').style.opacity = '1';
+    }, 500);
+    window.setTimeout(function(){
+        document.getElementById('tg-channel-button').style.visibility = 'visible';
+        document.getElementById('tg-channel-button').style.opacity = '1';
+    }, 1100);
+    window.setTimeout(function(){
+        document.getElementById('tg-group-button').style.visibility = 'visible';
+        document.getElementById('tg-group-button').style.opacity = '1';
+    }, 1800);
+    window.setTimeout(function(){
+        document.getElementById('wechat-button').style.visibility = 'visible';
+        document.getElementById('wechat-button').style.opacity = '1';
+    }, 2600);
+    window.setTimeout(function(){
+        document.getElementById('threads-button').style.visibility = 'visible';
+        document.getElementById('threads-button').style.opacity = '1';
+    }, 3400);
+    window.setTimeout(function(){
+        document.getElementById('github-button').style.visibility = 'visible';
+        document.getElementById('github-button').style.opacity = '1';
+    }, 4200);
+};
+
 switchHover()
