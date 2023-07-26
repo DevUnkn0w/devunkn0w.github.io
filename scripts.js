f81bdb03adfd421a3fdae8601dafc8e03023fdd3
zixi39 <dev.zixi@gmail.com>
2023-07-25 23:24:44 -0400

    change style

--- scripts.js
+++ scripts.js
@@ -25,50 +25,7 @@ function wechatInfo(){
     document.getElementById('navbar-image-box').style.left = 'calc(50% - '+logoWidth+'px)';
     document.getElementById('nav-back-button').style.visibility = 'visible';
     document.getElementById('nav-back-button').style.opacity = '1';
-    let htmlContent = `
-    <div class="box" id="wechat-panel-logo">
-        <div id = "wechat-panel-logo-icon-div">
-            <img src = "assets/icons/flower.svg" id="wechat-panel-logo-icon">
-        </div>
-        <div id = "wechat-panel-logo-text">
-            <b>梓</b>
-            <img src = "assets/icons/pipe.svg" id="wechat-panel-logo-text-divider">
-            <b>熙</b>
-        </div>
-    </div>
-    <div class="box wechat-method">
-        <div class = "wechat-method-title">
-            <img src = "assets/icons/shield-cross-fill.svg" class="wechat-method-title-icon">
-            <p><b>I</b></p>
-            <img src = "assets/icons/shield-cross-fill.svg" class="wechat-method-title-icon">
-        </div>
-        <div class = "wechat-method-content">
-            <p>参与以下互动</p>
-            <p>1. 积极点赞，转推并评论推文</p>
-            <p>2. 长期主动参与 <img src = "assets/icons/telegram.svg" class = "wechat-method-content-icon" id = "wechat-method-content-telegram-icon" > Telegram 群组中的讨论</p><br>
-            <p>经过一段时间的考察后，满足条件的用户将会在推特私信信箱中收到微信好友邀请</p>
-        </div>
-        <div class = "wechat-method-footer">
-            <img src = "assets/icons/cross.svg" class = "wechat-method-footer-icon">
-        </div>
-    </div>
-    <div class="box wechat-method">
-        <div class = "wechat-method-title">
-            <img src = "assets/icons/shield-cross-fill.svg" class="wechat-method-title-icon">
-            <p><b>II</b></p>
-            <img src = "assets/icons/shield-cross-fill.svg" class="wechat-method-title-icon">
-        </div>
-        <div class = "wechat-method-content">
-            <p style="font-family: Arial, Helvetica, sans-serif; text-align: center;"><b>Deprecated</b> <img src = "assets/icons/down.svg" class = "wechat-method-content-icon" id = "wechat-method-content-down-icon"></p><br>
-            <p class="deprecated">在推特上参与不定期举行的微信抽签活动，具体活动规则以及参与方式请参见抽签活动推文</p><br>
-            <p class="deprecated">抽签结果公布后，中签的用户将会在推特私信信箱中收到微信好友邀请</p>
-        </div>
-        <div class = "wechat-method-footer">
-            <img src = "assets/icons/cross.svg" class = "wechat-method-footer-icon">
-        </div>
-    </div>
-    `
-    document.getElementById('wechat-panel').innerHTML = htmlContent;
+    //document.getElementById('canvas').scrollTo({ top: 0, behavior: 'smooth' });
     document.getElementById('wechat-panel').style.left = '0';
     document.getElementById('home').style.transitionDuration = '1s';
     document.getElementById('home').style.transitionTimingFunction = 'ease';
@@ -87,9 +44,6 @@ function toHome(){
     document.getElementById('home').style.transitionDelay = '0.3s';
     document.getElementById('home').style.visibility = 'visible';
     document.getElementById('home').style.opacity = '1';
-    window.setTimeout(function () {
-        document.getElementById('wechat-panel').innerHTML = '';
-    }, 1500);
 }
 
 window.onload = function(){
